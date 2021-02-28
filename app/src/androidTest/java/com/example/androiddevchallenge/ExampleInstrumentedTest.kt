163c9e318cbde7b8d034237e2bdb122dac434ec9
Kwabena Berko <kwabenaberko95@gmail.com>
2021-02-28 20:48:05 +0000

    Experimental Annotation

--- app/src/androidTest/java/com/example/androiddevchallenge/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/androiddevchallenge/ExampleInstrumentedTest.kt
@@ -15,6 +15,7 @@
  */
 package com.example.androiddevchallenge
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
@@ -28,6 +29,7 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @ExperimentalFoundationApi
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
